97ce3e78bb2b710686a1be0e086e2c7c8bc21e75
Zaurbek Digurov <digurzaur@yandex.ru>
2024-03-18 23:30:10 +0300

    complete Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,7 @@
+﻿// Задача 2: Задайте строку, содержащую латинские буквы в 
+// обоих регистрах. Сформируйте строку, в которой 
+// все заглавные буквы заменены на строчные.
+System.Console.Write("Введите строку:");
+string StR = Console.ReadLine();
+string str = StR.ToLower();
+System.Console.WriteLine(str);
