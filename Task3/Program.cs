48dd804d75533189605853f4a13d1738eb784872
Zaurbek Digurov <digurzaur@yandex.ru>
2024-03-18 23:41:56 +0300

    complete Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 3: Задайте произвольную строку. 
+// Выясните, является ли она палиндромом.
+
+Console.WriteLine("Введите строку:");
+string input = Console.ReadLine();
+string reversedInput = new string(input.ToCharArray(), 0, input.Length);
+reversedInput = reversedInput.Replace(" ", "");
+if (string.Equals(reversedInput, new string(reversedInput.Reverse().ToArray()), StringComparison.OrdinalIgnoreCase))
+    {
+        Console.WriteLine("Да, это палиндром.");
+    }
+else
+    {
+        Console.WriteLine("Нет, это не палиндром.");
+    }
