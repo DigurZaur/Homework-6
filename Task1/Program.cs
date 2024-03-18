2c5b8c9caa4c48b80028da484bd9d22ebf1e6531
Zaurbek Digurov <digurzaur@yandex.ru>
2024-03-18 23:26:51 +0300

    complete Task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1: Задайте двумерный массив символов 
+// (тип char [,]). Создать строку из символов этого массива.
+
+char[,] array = { {'h', 'e', 'l', 'l', 'o'} , {'w', 'o', 'r', 'l', 'd'}};
+string str = "";
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        str = str + array[i,j];
+    }
+}
+
+System.Console.WriteLine(str);
