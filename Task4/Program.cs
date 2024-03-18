40f6df453a48c1c07a6503d08752a28a3b2fd6e7
Zaurbek Digurov <digurzaur@yandex.ru>
2024-03-18 23:44:06 +0300

    complete Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 4*(не обязательная): Задайте строку, состоящую из слов, 
+// разделенных пробелами. Сформировать строку, в которой слова расположены
+// в обратном порядке. В полученной строке слова должны быть также разделены пробелами.
+
+Console.WriteLine("Введите строку из слов, разделенных пробелами:");
+string input = Console.ReadLine();
+string[] words = input.Split(' ');
+Array.Reverse(words);
+string reversedString = string.Join(" ", words);
+Console.WriteLine("Результат: " + reversedString);
